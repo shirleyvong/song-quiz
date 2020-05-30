76ae9042b031e8378253a116f3dbb4ade3cd47f0
Shirley Vong <shirleyv97@hotmail.com>
2020-05-29 17:18:36 -0700

    Allow Button component to accept a visibility prop

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,30 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Button = ({ handleClick, text }) => {
+const Button = ({ handleClick, text, isVisible = true }) => {
+  const Btn = styled.button`
+    height: 40px;
+    padding: 10px 20px;
+    margin: 20px;
+    font-family: inherit;
+    background-color: white;
+    border: none;
+    border-radius: 500px;
+    font-weight: bold;
+    visibility: ${isVisible ? 'visible' : 'hidden'};
+    
+    &:hover {
+      cursor: pointer;
+    }
+
+    &:focus {
+      outline: none;
+    }
+  `;
+
   return (
     <Btn type="button" onClick={handleClick}>{text}</Btn>
   );
 };
-
-const Btn = styled.button`
-  height: 40px;
-  padding: 10px 20px;
-  margin: 20px;
-  font-family: inherit;
-  background-color: white;
-  border: none;
-  border-radius: 500px;
-  font-weight: bold;
-
-  &:hover {
-    cursor: pointer;
-  }
-
-  &:focus {
-    outline: none;
-  }
-`;
 
 
 export default Button;
