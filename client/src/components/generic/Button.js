486ba66d15bd84aa056d2c7b3c04d7729742ecac
Shirley Vong <shirleyv97@hotmail.com>
2020-06-06 15:39:49 -0700

    Move styling for Button outside component

--- client/src/components/generic/Button.js
+++ client/src/components/generic/Button.js
@@ -2,30 +2,30 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Button = ({ handleClick, text, isVisible = true }) => {
-  const Btn = styled.button`
-    height: 40px;
-    padding: 10px 20px;
-    margin: 10px;
-    font-family: inherit;
-    background-color: white;
-    border: none;
-    border-radius: 500px;
-    font-weight: bold;
-    visibility: ${isVisible ? 'visible' : 'hidden'};
-    
-    &:hover {
-      cursor: pointer;
-    }
-
-    &:focus {
-      outline: none;
-    }
-  `;
-
   return (
-    <Btn type="button" onClick={handleClick}>{text}</Btn>
+    <Btn type="button" onClick={handleClick} visible={isVisible}>{text}</Btn>
   );
 };
+
+const Btn = styled.button`
+  height: 40px;
+  padding: 10px 20px;
+  margin: 10px;
+  font-family: inherit;
+  background-color: white;
+  border: none;
+  border-radius: 500px;
+  font-weight: bold;
+  visibility: ${(props) => props.visible ? 'visible' : 'hidden' };
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  &:focus {
+    outline: none;
+  }
+`;
 
 
 export default Button;
