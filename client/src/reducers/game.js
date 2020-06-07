f238406b6c2084543966fd4bcd8090a8494b200a
Shirley Vong <shirleyv97@hotmail.com>
2020-06-06 22:48:31 -0700

    Create actions creators and reducers for game logic

--- client/src/reducers/game.js
+++ client/src/reducers/game.js
@@ -1,8 +1,15 @@
+import shuffle from 'knuth-shuffle-seeded';
+import api from '../services/api';
+
 const SET_TRACKS = 'SET_TRACKS';
 const INCREMENT_ROUND = 'INCREMENT_ROUND';
 const SET_QUESTIONS = 'SET_QUESTIONS';
 const INCREMENT_CORRECT = 'INCREMENT_CORRECT';
 const SET_GAME_STATE = 'SET_GAME_STATE';
+const RESET_GAME = 'RESET_GAME';
+const FETCH_TRACKS_PENDING = 'FETCH_TRACKS_PENDING';
+const FETCH_TRACKS_SUCCESS = 'FETCH_TRACKS_SUCCESS';
+const FETCH_TRACKS_FAILURE = 'FETCH_TRACKS_FAILURE';
 
 const GAME_STATE = {
   LOADING: 'LOADING',
@@ -16,6 +23,7 @@ const initialState = {
   roundNum: 0,
   numCorrect: 0,
   gameState: GAME_STATE.LOADING,
+  isLoading: false,
 };
 
 const reducer = (state = initialState, action) => {
@@ -30,6 +38,19 @@ const reducer = (state = initialState, action) => {
       return { ...state, numCorrect: state.numCorrect + 1 };
     case SET_GAME_STATE:
       return { ...state, gameState: state.gameState };
+    case RESET_GAME:
+      return {
+        ...state,
+        questions: [],
+        roundNum: 0,
+        numCorrect: 0,
+      };
+    case FETCH_TRACKS_PENDING:
+      return { ...state, isLoading: true };
+    case FETCH_TRACKS_SUCCESS:
+      return { ...state, isLoading: false, tracks: action.tracks };
+    case FETCH_TRACKS_FAILURE:
+      return { ...state, isLoading: true };
     default:
       return state;
   }
@@ -67,5 +88,89 @@ export const setGameState = (gameState) => {
     gameState,
   };
 };
+
+export const resetGame = () => {
+  return {
+    type: RESET_GAME,
+  };
+};
+
+export const fetchTracksPending = (artistId) => {
+  return {
+    type: FETCH_TRACKS_PENDING,
+    artistId,
+  };
+};
+
+export const fetchTracksSuccess = (tracks) => {
+  return {
+    type: FETCH_TRACKS_SUCCESS,
+    tracks,
+  };
+};
+
+export const fetchTracksFailure = () => {
+  return {
+    type: FETCH_TRACKS_FAILURE,
+  };
+};
+
+export const createQuestions = () => {
+  return async (dispatch, getState) => {
+    const NUM_QUESTIONS = 1;
+    const CHOICES_PER_QUESTION = 4;
+
+    const getRandomIndex = (maxNum) => Math.floor(Math.random() * maxNum - 1) + 1;
+    const { tracks } = getState().game;
+    const copiedTracks = [...tracks];
+    shuffle(copiedTracks);
+
+    const questions = [];
+    for (let i = 0; i < NUM_QUESTIONS; i++) {
+      const choices = [];
+      for (let j = 0; j < CHOICES_PER_QUESTION; j++) {
+        const idx = i * CHOICES_PER_QUESTION + j;
+        choices.push(copiedTracks[idx]);
+      }
+
+      const randIdx = getRandomIndex(CHOICES_PER_QUESTION);
+      const q = {
+        correctId: choices[randIdx].id,
+        choices,
+      };
+
+      questions.push(q);
+    }
+
+    dispatch(setQuestions(questions));
+  };
+};
+
+export const createGame = (artistId) => {
+  return async (dispatch) => {
+    dispatch(fetchTracksPending());
+
+    try {
+      const tracks = await api.getTracks(artistId);
+      dispatch(fetchTracksSuccess(tracks));
+      dispatch(createQuestions());
+    } catch (error) {
+      dispatch(fetchTracksFailure());
+    }
+  };
+};
+
+// export const replayGame = () => {
+//   return async (dispatch) => {
+//     dispatch(fetchTracksPending());
+
+//     try {
+//       const tracks = api.getTracks(artistId);
+//       dispatch(fetchTracksSuccess(tracks));
+//     } catch (error) {
+//       dispatch(fetchTracksFailure());
+//     }
+//   };
+// };
 
 export default reducer;
--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,12 @@
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { createStore } from 'redux';
 import rootReducer from './reducers';
 
 
-export default createStore(rootReducer, composeWithDevTools());
+export default createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(thunk),
+  ),
+);
