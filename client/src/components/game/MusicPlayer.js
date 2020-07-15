82c555f192068488b3979b34f4afb5c924c4d8cb
Shirley Vong <shirleyv97@hotmail.com>
2020-07-14 22:40:12 -0700

    Fix to pause music properly on url change and unmount

--- client/src/components/game/MusicPlayer.js
+++ client/src/components/game/MusicPlayer.js
@@ -1,31 +1,46 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { FaPlay, FaPause } from 'react-icons/fa';
 
 const MusicPlayer = ({ url }) => {
-  const [audio] = useState(new Audio(url));
+  const [audio, setAudio] = useState(new Audio(url));
   const [isPlaying, setIsPlaying] = useState(false);
 
-  useEffect(() => {
-    if (isPlaying) {
-      audio.play();
-    } else {
-      audio.pause();
-    }
-  }, [isPlaying]);
+  const latestUrl = useRef(url);
+  const latestAudio = useRef(audio);
 
   useEffect(() => {
-    audio.addEventListener('ended', () => setIsPlaying(false));
-
-    return () => {
+    // Only run this code if the url changes or else we get an infinite loop
+    // since we need to keep audio in dependencies
+    if (url !== latestUrl.current) {
+      // The old audio won't become garbage collected until it's paused, so manually pause
+      // before setting new audio element.
       audio.pause();
-      audio.removeEventListener('ended', () => setIsPlaying(false));
       setIsPlaying(false);
+      setAudio(new Audio(url));
+      latestUrl.current = url;
+    }
+  }, [url, audio]);
+
+  useEffect(() => {
+    latestAudio.current = audio;
+  }, [audio]);
+
+  useEffect(() => {
+    return () => {
+      // Can't do audio.pause() because in this effect, audio refers to the first audio element
+      // that was created. Must pause the reference for the latest audio element.
+      latestAudio.current.pause();
     };
-  }, [url]);
+  }, []);
 
   const handleButtonClick = () => {
+    if (isPlaying) {
+      audio.pause();
+    } else {
+      audio.play();
+    }
     setIsPlaying(!isPlaying);
   };
 
