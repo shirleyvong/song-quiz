3d66a6ca5bfdeda171a65adbf38bcb823e133ee5
Shirley Vong <shirleyv97@hotmail.com>
2020-07-15 02:40:44 -0700

    Add routes to get an access and refresh token from Spotify API
    If the user doesn't have a refresh token, they can retrieve the refresh token by calling /auth, which will eventually return a JSON object containing the access token and refresh token. The refresh token should be specified in the .env as REFRESH_TOKEN to automatically retrieve an access token.

--- server/app.js
+++ server/app.js
@@ -4,12 +4,17 @@ const cors = require('cors');
 const morgan = require('morgan');
 const middleware = require('./utils/middleware');
 const routes = require('./routes/index');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
+
+// Only used to get refresh token from Spotify API. Once you have the refresh token,
+// add REFRESH_TOKEN to .env and comment this line
+// app.use('/', authRoutes);
 
 app.use('/api', routes);
 
