99412f113f513f1f004619b0a55e4f27b544c868
Shirley Vong <shirleyv97@hotmail.com>
2020-07-20 01:04:31 -0700

    Remove redux devtools import

--- client/src/store.js
+++ client/src/store.js
@@ -1,12 +1,8 @@
 import thunk from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers';
-
 
 export default createStore(
   rootReducer,
-  composeWithDevTools(
-    applyMiddleware(thunk),
-  ),
+  applyMiddleware(thunk),
 );
